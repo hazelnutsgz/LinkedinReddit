7f2580007fcdbe3f2a084e348c3e5fd595bdd071
hazelnutsgz <guozhen.she@intel.com>
2018-10-31 16:31:38 +0800

    Update data

--- linkedin/profile/scrape_profile.py
+++ linkedin/profile/scrape_profile.py
@@ -15,6 +15,8 @@ print ("driver got??")
 with open("result_dict.json", "r") as fp:
     result = json.loads(fp.read())
 
+
+time.sleep(40)		
 count = 0
 for url in result:
 	trim_url = url.split('/')[-2]
@@ -24,18 +26,19 @@ for url in result:
 		continue
 
 
-	# driver.get(url)
-	# time.sleep(5)
-	# driver.execute_script("window.scrollTo(0, document.body.scrollHeight*0.5)")
-	# time.sleep(20)
-	# driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-	# time.sleep(20)
+
+	driver.get(url)
+	time.sleep(5)
+	driver.execute_script("window.scrollTo(0, document.body.scrollHeight*0.5)")
+	time.sleep(20)
+	driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+	time.sleep(20)
 	
 
-	# with open("../html/" + trim_url + ".html", 'w') as fp:
-	# 	fp.write(driver.page_source)
+	with open("../html/" + trim_url + ".html", 'w') as fp:
+		fp.write(driver.page_source)
 
 
 	print (count)
-	time.sleep(10)
+	time.sleep(30)
 	count += 1
